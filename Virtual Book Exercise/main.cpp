99ac98411b4d4eac546e9c8b0e996f6fdc770cf9
aberry5621 <aberry5621@gmail.com>
2017-04-06 10:27:45 -0700

    Initial Commit

---
+++ Virtual Book Exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Virtual Book Exercise
+//
+//  Created by ax on 4/6/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
